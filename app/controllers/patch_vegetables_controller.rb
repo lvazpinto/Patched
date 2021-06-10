ef8bb67fef41f0e9581e47b4bf478850f5c1664d
Patricia Nunes <patricia.amaral.nunes@hotmail.com>
2021-06-10 18:23:38 +0100

    veggiesselection

--- app/controllers/patch_vegetables_controller.rb
+++ app/controllers/patch_vegetables_controller.rb
@@ -1,10 +1,14 @@
 class PatchVegetablesController < ApplicationController
   def create
-
     params["patch_vegetable"]["vegetable"].each do |id|
-      selected_vegetable = Vegetable.find(id.to_i) if id.to_i != 0
-     # patchvegetable = PatchVegetable.new(vegetable: selected_vegetable, patch: )
+      if id.to_i != 0
+       selected_vegetable = Vegetable.find(id.to_i)
+       @current_patch = Patch.find(params["patch_id"].to_i)
+       patchvegetable = PatchVegetable.new(vegetable: selected_vegetable, patch: @current_patch)
+       patchvegetable.save
+      end
     end
+    redirect_to patch_path(@current_patch)
 
   end
 end
--- test/controllers/patchvegetables_controller_test.rb
+++ test/controllers/patchvegetables_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class PatchvegetablesControllerTest < ActionDispatch::IntegrationTest
+class PatchVegetablesControllerTest < ActionDispatch::IntegrationTest
   test "should get create" do
     get patchvegetables_create_url
     assert_response :success
