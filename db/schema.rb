c38a4376f7bcd444ea5c3d776844d2ef60e31bcf
mariana saavedra <marianadsbs@gmail.com>
2021-06-15 12:16:06 +0100

    Functionality of harvest page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_10_154115) do
+ActiveRecord::Schema.define(version: 2021_06_15_105240) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,7 +57,6 @@ ActiveRecord::Schema.define(version: 2021_06_10_154115) do
     t.bigint "patch_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "status", default: "planted"
     t.index ["patch_id"], name: "index_patch_vegetables_on_patch_id"
     t.index ["vegetable_id"], name: "index_patch_vegetables_on_vegetable_id"
   end
